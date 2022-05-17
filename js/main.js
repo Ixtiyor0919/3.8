c1ba85ae287be07555a9651de8a05e5073fcf301
Ixtiyor0919 <Komiljonov2002@outlook.com>
2022-05-17 13:01:07 +0500

    mukammal

--- js/main.js
+++ js/main.js
@@ -3,55 +3,37 @@ var distanceInput = distanceForm.querySelector('.form__input');
 var distanceFoot = distanceForm.querySelector('.form__foot');
 var distanceBicycle = distanceForm.querySelector('.form__bicycle');
 var distanceCar = distanceForm.querySelector('.form__car');
-var distancePlan = distanceForm.querySelector('.form__plan');
-var distanceFootMinutes = distanceForm.querySelector('.form__foot__minutes');
-var distanceBicycleMinutes = distanceForm.querySelector('.form__bicycle__minutes');
-var distanceCarMinutes = distanceForm.querySelector('.form__car__minutes');
-var distancePlanMinutes = distanceForm.querySelector('.form__plan__minutes');
+var distancePlane = distanceForm.querySelector('.form__plan');
 
     var onFoot = 3.6;
     var bicycle = 20.1;
     var car = 70;
-    var plan = 800;
+    var plane = 800;
+    
+    function calculateTime(distance, speed) {
+        const hour = Math.floor(distance / speed);
+        const minute = Math.floor((distance / speed -hour) *60)
+        const second = Math.round(((distance / speed -hour) * 60 -minute) *60)
+        return hour + ' soat ' + minute + 'minut' + second + 'sekund'
+    }
     
     distanceForm.addEventListener('submit', function(evt) {
         evt.preventDefault();
         distanceInput.value.trim();
-        var distance = distanceInput.value;
-        
-        var footHour = Math.floor(distance / onFoot);
-        var bicycleHour = Math.floor(distance / bicycle);
-        var carHour = Math.floor(distance / car);
-        var planHour = Math.floor(distance / plan);
 
-        distanceFoot.textContent = footHour + ' soat ';
-        distanceBicycle.textContent = bicycleHour + ' soat ';
-        distanceCar.textContent = carHour + ' soat ';
-        distancePlan.textContent = planHour + ' soat ';
+        distanceFoot.textContent = calculateTime(distanceInput.value, onFoot);
+        distanceBicycle.textContent = calculateTime(distanceInput.value, bicycle);
+        distanceCar.textContent = calculateTime(distanceInput.value, car);
+        distancePlane.textContent = calculateTime(distanceInput.value, plane);
 
-        var footMinute = Math.floor((distance / onFoot - footHour) * 60);
-        var bicycleMinute = Math.floor((distance / bicycle - bicycleHour) * 60);
-        var carMinute = Math.floor((distance / car - carHour) * 60);
-        var planMinute = Math.floor((distance / plan - planHour) * 60);
+        if (distanceInput.value <= 0 || isNaN(distanceInput.value)) {
+            distanceInput.classList.add("form__input--invalid")
+            distanceInput.classList.remove("form__input--valid")
+            return
+        }else {
+            distanceInput.classList.add("form__input--valid")
+             distanceInput.classList.remove("form__input--invalid")
+            return
+        };
 
-        distanceFootMinutes.textContent = footMinute + ' daqiqa ';
-        distanceBicycleMinutes.textContent = bicycleMinute + ' daqiqa ';
-        distanceCarMinutes.textContent = carMinute + ' daqiqa ';
-        distancePlanMinutes.textContent = planMinute + ' daqiqa ';
-
-    // distanceFoot.textContent = `${Math.floor(onFoot / (onFoot))} soat`; 
-    // distanceBicycle.textContent = `${(distance / (bicycle))} soat`; 
-    // distanceCar.textContent = `${(distance / (carHour))} soat`; 
-    // distancePlan.textContent = `${(distance / (planHour))} soat`;
-    
-    
-
-    distanceFoot.style.color = 'rgb(138 137 139)';
-    distanceBicycle.style.color = 'rgb(138 137 139)';
-    distanceCar.style.color = 'rgb(138 137 139)';
-    distancePlan.style.color = 'rgb(138 137 139)';
-    distanceFootMinutes.style.color = 'rgb(138 137 139)';
-    distanceBicycleMinutes.style.color = 'rgb(138 137 139)';
-    distanceCarMinutes.style.color = 'rgb(138 137 139)';
-    distancePlanMinutes.style.color = 'rgb(138 137 139)';
-})
+    });
